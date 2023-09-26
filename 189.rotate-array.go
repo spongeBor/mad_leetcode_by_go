790a9ba7de8e053232db77f4408e7cf3e8e43843
spongeB <spongeBor@proton.me>
2023-09-26 23:21:02 +0800

    feat; add 189

---
+++ 189.rotate-array.go
@@ -0,0 +1,35 @@
+package lc
+
+/*
+ * @lc app=leetcode.cn id=189 lang=golang
+ * @lcpr version=21917
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+func rotate(nums []int, k int) {
+	k %= len(nums)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5,6,7]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,-100,3,99]\n2\n
+// @lcpr case=end
+
+*/
