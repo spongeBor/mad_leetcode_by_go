471b525ce041869a853fd5e9a616f867a845dd8c
spongeB <spongeBor@proton.me>
2023-09-27 23:45:51 +0800

    feat: add 45

---
+++ 45.jump-game-ii.go
@@ -0,0 +1,44 @@
+package lc
+
+/*
+ * @lc app=leetcode.cn id=45 lang=golang
+ * @lcpr version=21917
+ *
+ * [45] 跳跃游戏 II
+ */
+
+// @lc code=start
+func jump(nums []int) int {
+	length := len(nums)
+	end := 0
+	maxPosition := 0
+	steps := 0
+	for i := 0; i < length-1; i++ {
+		maxPosition = max(maxPosition, i+nums[i])
+		if i == end {
+			end = maxPosition
+			steps++
+		}
+	}
+	return steps
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [2,3,1,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,3,0,1,4]\n
+// @lcpr case=end
+
+*/
