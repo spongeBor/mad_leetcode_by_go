92fd6dfc5d019d3ae69b0bb61dfab2c41b798854
spongeB <spongeBor@proton.me>
2023-09-29 20:50:46 +0800

    add 605

---
+++ 605.can-place-flowers.go
@@ -0,0 +1,47 @@
+package lc
+
+/*
+ * @lc app=leetcode.cn id=605 lang=golang
+ * @lcpr version=21917
+ *
+ * [605] 种花问题
+ */
+
+// @lc code=start
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	count := 0
+	m := len(flowerbed)
+	prev := -1
+	for i := 0; i < m; i++ {
+		if flowerbed[i] == 1 {
+			if prev < 0 {
+				count += i / 2
+			} else {
+				count += (i - prev - 2) / 2
+			}
+			if count >= n {
+				return true
+			}
+			prev = i
+		}
+	}
+	if prev < 0 {
+		count += (m + 1) / 2
+	} else {
+		count += (m - prev - 1) / 2
+	}
+	return count >= n
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,0,0,0,1]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,0,0,0,1]\n2\n
+// @lcpr case=end
+
+*/
