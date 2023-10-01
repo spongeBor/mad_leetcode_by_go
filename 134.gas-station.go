c1509979dcca4a964f394795c6f13a0c60f71606
spongeB <spongeBor@proton.me>
2023-10-01 22:04:06 +0800

    feat: add 134

---
+++ 134.gas-station.go
@@ -0,0 +1,43 @@
+package lc
+
+/*
+ * @lc app=leetcode.cn id=134 lang=golang
+ * @lcpr version=21917
+ *
+ * [134] 加油站
+ */
+
+// @lc code=start
+func canCompleteCircuit(gas []int, cost []int) int {
+	for i, n := 0, len(gas); i < n; {
+		sumOfGas, sumOfCost, cnt := 0, 0, 0
+		for cnt < n {
+			j := (i + cnt) % n
+			sumOfGas += gas[j]
+			sumOfCost += cost[j]
+			if sumOfCost > sumOfGas {
+				break
+			}
+			cnt++
+		}
+		if cnt == n {
+			return i
+		} else {
+			i += cnt + 1
+		}
+	}
+	return -1
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n[3,4,5,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,3,4]\n[3,4,3]\n
+// @lcpr case=end
+
+*/
