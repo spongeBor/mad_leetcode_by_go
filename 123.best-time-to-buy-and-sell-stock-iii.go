a8f822ed5dfb30ba844ab5fd79b6db77cd353584
spongeB <spongeBor@proton.me>
2023-10-03 19:06:05 +0800

    feat: add 123

---
+++ 123.best-time-to-buy-and-sell-stock-iii.go
@@ -0,0 +1,49 @@
+package lc
+
+/*
+ * @lc app=leetcode.cn id=123 lang=golang
+ * @lcpr version=21917
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+func maxProfit3(prices []int) int {
+	buy1, sell1 := -prices[0], 0
+	buy2, sell2 := -prices[0], 0
+	for i := 1; i < len(prices); i++ {
+		buy1 = max(buy1, -prices[i])
+		sell1 = max(sell1, buy1+prices[i])
+		buy2 = max(buy2, sell1-prices[i])
+		sell2 = max(sell2, buy2+prices[i])
+	}
+	return sell2
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [3,3,5,0,0,3,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,6,4,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+*/
