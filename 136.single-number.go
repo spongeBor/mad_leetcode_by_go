f3ecee4a4831443fa602a1142ff9fab327bc353d
spongeB <spongeBor@proton.me>
2023-10-14 22:54:13 +0800

    feat: add 136

---
+++ 136.single-number.go
@@ -0,0 +1,34 @@
+package lc
+
+/*
+ * @lc app=leetcode.cn id=136 lang=golang
+ * @lcpr version=21917
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+func singleNumber(nums []int) int {
+	single := 0
+	for _, num := range nums {
+		single ^= num
+	}
+	return single
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [2,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,1,2,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+*/
